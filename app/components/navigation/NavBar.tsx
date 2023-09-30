972b0821bc128d665caa5cd10e90cb76eded005d
jmwalsh91 <jordanmwalsh@gmail.com>
2023-09-30 17:37:23 -0600

    task: dropdown links mobile

--- app/components/navigation/NavBar.tsx
+++ app/components/navigation/NavBar.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Icon from "../icons/icon";
-import { Link } from "@remix-run/react";
+import { Link, useLocation } from "@remix-run/react";
 
 type Props = {};
 
 export default function NavBar({}: Props) {
+  const { pathname } = useLocation();
+
+  /*   function navLinks() {
+    if (pathname.includes("login") || pathname.includes("dash")) {
+      return <Link to="/">Home</Link>;
+    } else {
+      return (
+        <>
+          <a href="#about">About</a>
+          <a href="#tools">Tools</a>
+          <a href="#contact">Contact</a>
+        </>
+      );
+    }
+  } */
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -29,33 +44,31 @@ export default function NavBar({}: Props) {
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-           {/*  <li>
-              <a>Item 1</a>
-            </li>
+            <li><a href="/">Home</a></li>
+
             <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
+              <a href="/login">Sign In</a>
             </li>
-            <li>
-              <a>Item 3</a>
-            </li> */}
           </ul>
         </div>
-        <a  href="/" className="btn btn-ghost normal-case text-xl">     <Icon icon="tapCyan" width={60} height={60} color="cyan" className="bg-cyan-300 rounded-xl" />TaskAutomationPartners</a>
+        <a href="/" className="btn btn-ghost normal-case text-xl">
+          {" "}
+          <Icon
+            icon="tapCyan"
+            width={60}
+            height={60}
+            color="cyan"
+            className="bg-cyan-300 rounded-xl"
+          />
+          TaskAutomationPartners
+        </a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-         {/*  <li>
+          {/*  <li>
             <a>Item 1</a>
           </li> */}
-        {/*   <li tabIndex={0}>
+          {/*   <li tabIndex={0}>
             <details>
               <summary>Parent</summary>
               <ul className="p-2">
@@ -73,8 +86,10 @@ export default function NavBar({}: Props) {
           </li> */}
         </ul>
       </div>
-      <div className="navbar-end">
-    <Link to="/login" className="btn">Sign In</Link>
+      <div className="navbar-end hidden lg:flex">
+        <Link to="/login" className="btn">
+          Sign In
+        </Link>
       </div>
     </div>
   );
--- app/components/sections/ToolSection.tsx
+++ app/components/sections/ToolSection.tsx
@@ -17,50 +17,50 @@ type ToolsWithIconsType = {
   [title: string]: React.ReactNode;
 };
 
-const toolIcons = {
-    "Robust Backend Foundations": (
-      <div className="p-2 shadow-lg bg-gradient-to-r from-blue-500 to-cyan-500 rounded-md inline-block">
-        <IconBrandCSharp className="text-white" size={48} />
+const toolIcons: ToolsWithIconsType = {
+  "Robust Backend Foundations": (
+    <div className="p-2 shadow-lg bg-gradient-to-r from-blue-500 to-cyan-500 rounded-md inline-block">
+      <IconBrandCSharp className="text-white" size={48} />
+    </div>
+  ),
+  "Dynamic Frontend Experiences": (
+    <div className="p-2 shadow-lg bg-gradient-to-r from-purple-400 to-indigo-500 rounded-md inline-block">
+      <IconBrandReact className="text-white" size={48} />
+    </div>
+  ),
+  "Flexible Hosting Solutions": (
+    <span className="flex gap-1 h-14">
+      <div className="p-2 shadow-lg bg-gradient-to-r from-orange-400 to-yellow-500 rounded-md inline-block">
+        <IconBrandAws className="text-black" size={48} />
       </div>
-    ),
-    "Dynamic Frontend Experiences": (
-      <div className="p-2 shadow-lg bg-gradient-to-r from-purple-400 to-indigo-500 rounded-md inline-block">
-        <IconBrandReact className="text-white" size={48} />
-      </div>
-    ),
-    "Flexible Hosting Solutions": (
-      <span className="flex gap-1 h-14">
-        <div className="p-2 shadow-lg bg-gradient-to-r from-orange-400 to-yellow-500 rounded-md inline-block">
-          <IconBrandAws className="text-black" size={48} />
-        </div>
-        <div className="p-2 shadow-lg bg-gradient-to-r from-blue-400 to-teal-500 rounded-md inline-block">
-          <IconBrandAzure className="text-white" size={48} />
-        </div>
-        <div className="p-2 shadow-lg bg-gradient-to-r from-blue-400 to-purple-500 rounded-md inline-block">
-  <IconBrandDocker className="text-white" size={48} />
-</div>
-      </span>
-    ),
-    "Comprehensive Project Management": (
-      <div className="p-2 shadow-lg bg-gradient-to-r from-green-400 to-teal-500 rounded-md inline-block">
+      <div className="p-2 shadow-lg bg-gradient-to-r from-blue-400 to-teal-500 rounded-md inline-block">
         <IconBrandAzure className="text-white" size={48} />
       </div>
-    ),
-    "Adaptive Content Management": (
-      <div className="p-2 shadow-lg bg-gradient-to-r from-gray-400 to-gray-500 rounded-md inline-block">
-        <IconDeviceDesktop className="text-white" size={48} />
+      <div className="p-2 shadow-lg bg-gradient-to-r from-blue-400 to-purple-500 rounded-md inline-block">
+        <IconBrandDocker className="text-white" size={48} />
       </div>
-    ),
-    "Security and Compliance Excellence": (
-      <div className="p-2 shadow-lg bg-gradient-to-r from-red-500 to-pink-500 rounded-md inline-block">
-        <IconLockCode className="text-white" size={48} />
-      </div>
-    ),
-  };
-  
+    </span>
+  ),
+  "Comprehensive Project Management": (
+    <div className="p-2 shadow-lg bg-gradient-to-r from-green-400 to-teal-500 rounded-md inline-block">
+      <IconBrandAzure className="text-white" size={48} />
+    </div>
+  ),
+  "Adaptive Content Management": (
+    <div className="p-2 shadow-lg bg-gradient-to-r from-gray-400 to-gray-500 rounded-md inline-block">
+      <IconDeviceDesktop className="text-white" size={48} />
+    </div>
+  ),
+  "Security and Compliance Excellence": (
+    <div className="p-2 shadow-lg bg-gradient-to-r from-red-500 to-pink-500 rounded-md inline-block">
+      <IconLockCode className="text-white" size={48} />
+    </div>
+  ),
+};
+
 export default function ToolSection({}: Props) {
   return (
-    <div className="container mx-auto py-12">
+    <div id="tools" className="container mx-auto py-12">
       <div className="mb-8 text-center">
         <h2 className="focus-visible:animate-pulse text-4xl font-bold text-cyan-200">
           Empowering Your Business with Cutting-Edge Technologies
